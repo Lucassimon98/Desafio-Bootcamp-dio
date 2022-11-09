ff120bea45839c6ce352dd282424212a1e41ccde
Lucas Henrique Simon <49909759+Lucassimon98@users.noreply.github.com>
2022-11-09 11:16:31 -0300

    Criada as classes Mentoria e Curso

--- src/App.java
+++ src/App.java
@@ -1,5 +1,31 @@
+import java.time.LocalDate;
+
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descrição");
+        curso1.setCargaHoraria(10);
+        
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso JS");
+        curso2.setDescricao("Descrição");
+        curso2.setCargaHoraria(12);
+
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+
+        Mentoria mentoria1 = new Mentoria();
+
+        mentoria1.setTitulo("Java Powered");
+        mentoria1.setDescricao("Descrição");
+        mentoria1.setData(LocalDate.now());
+
+        System.out.println(mentoria1);
     }
 }
